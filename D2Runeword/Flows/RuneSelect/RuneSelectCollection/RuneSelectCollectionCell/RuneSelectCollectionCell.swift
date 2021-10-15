1c5b046610c38875d66b3bdda5ec0a644c12b9d6
Artem Kayumov <kayumov.artem@gmail.com>
2021-10-15 22:53:23 +0500

    Add .gitignore file

---
+++ D2Runeword/Flows/RuneSelect/RuneSelectCollection/RuneSelectCollectionCell/RuneSelectCollectionCell.swift
@@ -0,0 +1,12 @@
+//
+//  RuneSelectCollectionCell.swift
+//  D2Runeword
+//
+//  Created by Artem Kayumov on 15.10.2021.
+//
+
+import UIKit
+
+class RuneSelectCollectionCell: UICollectionViewCell {
+    
+}
